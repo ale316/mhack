8dc9b750079a0e0204875f6fa476fbc6443b73bb
Zak Semenov <zak.semenov@gmail.com>
2013-09-21 16:04:08 -0400

    stupid fb now gives friends ... at the expense of a lot of pain/stupidity, sorry guys :(

--- chrome/feader/background.js
+++ chrome/feader/background.js
@@ -5,7 +5,7 @@ function onFacebookLogin() {
             for (var i = 0; i < tabs.length; i++) {
                 if (tabs[i].url.indexOf(successURL) == 0) {
                     var params = tabs[i].url.split('#')[1];
-	access = params.split('&')[0]
+	               access = params.split('&')[0]
                     console.log(access);
                     localStorage.accessToken = access;
                     chrome.tabs.onUpdated.removeListener(onFacebookLogin);
--- chrome/feader/fb_success.js
+++ chrome/feader/fb_success.js
@@ -1,4 +1,12 @@
-var msg = localStorage ? JSON.stringify(localStorage) : "no utoken";
-alert(msg);
+var params = document.URL.split('#')[1];
+var utoken = params.split('&')[0];
 
-window.close();
+// window.close();
+
+var data = $.get(addToken('https://graph.facebook.com/me/friends'), function(res){
+	console.log(res);
+});
+
+ function addToken(url){
+ 	return url = url + '?' + utoken; 
+ }
