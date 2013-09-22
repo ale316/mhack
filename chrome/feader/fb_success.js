0933652c794c5985de5d95edca68376d11e03202
Zak Semenov <zak.semenov@gmail.com>
2013-09-21 21:36:14 -0400

    stuff

--- chrome/feader/fb_success.js
+++ chrome/feader/fb_success.js
@@ -1,12 +1,29 @@
 var params = document.URL.split('#')[1];
 var utoken = params.split('&')[0];
 
-// window.close();
+// need to store the utoken in a cookie
 
-var data = $.get(addToken('https://graph.facebook.com/me/friends'), function(res){
-	console.log(res);
+window.close();
+
+var friends, _id, pic, data;
+
+var pic_url = addToken('https://graph.facebook.com/me') + '&fields=picture';
+
+$.get(addToken('https://graph.facebook.com/me/friends'), function(res){
+	friends = res.data;
+	console.log(friends);
+	$.get(pic_url, function(res){
+		_id = res.id;
+		pic = res.picture.data.url;
+		console.log(_id,pic);
+		data = { id : _id, friends: friends, picture : pic };
+		
+		var socket = io.connect('http://localhost');
+		socket.emit('user_log', data);
+	});
 });
 
- function addToken(url){
- 	return url = url + '?' + utoken; 
- }
+function addToken(url){
+	return url = url + '?' + utoken; 
+}
+
